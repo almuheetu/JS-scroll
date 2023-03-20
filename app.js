efd05e95a2502463fc68f6a4185f7407889eef25
almuheetu <114015068+almuheetu@users.noreply.github.com>
2023-03-20 22:41:05 +0600

    JS fix

--- app.js
+++ app.js
@@ -9,17 +9,22 @@ const date = document.getElementById("date");
 date.innerHTML = new Date().getFullYear();
 
 // ********** close links ************
-const navToggle = document.querySelector('.nav-toggle');
-const linksContainer = Document.querySelector(".links-Container");
+const navToggle = document.querySelector(".nav-toggle");
+const linksContainer = document.querySelector(".links-container");
 const links = document.querySelector(".links");
 
-navToggle.addEventListener("click", function() {
+navToggle.addEventListener("click", function () {
     // linksContainer.classList.toggle("show-links");
-    const ContainerHeight = linksContainer.getBoundingClientRect().height;
+  
     const linksHeight = links.getBoundingClientRect().height;
-    console.log(linksHeight);
-
-});
+    const containerHeight = linksContainer.getBoundingClientRect().height;
+    if (containerHeight === 0) {
+      linksContainer.style.height = `${linksHeight}px`;
+    } else {
+      linksContainer.style.height = 0;
+    }
+    // console.log(linksContainer.getBoundingClientRect());
+  });
 // ********** fixed navbar ************
 
 // ********** smooth scroll ************
